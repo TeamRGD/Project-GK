443093029c54d03ec719f6f71675579873592789
CuriHuS <2542656@naver.com>
2024-08-10 01:14:55 +0900

    Fix: PlayerRaycast script, player prefab

--- Project-GK/Assets/HS/Scripts/PlayerRaycast.cs
+++ Project-GK/Assets/HS/Scripts/PlayerRaycast.cs
@@ -6,17 +6,19 @@ public class PlayerRaycast : MonoBehaviour
 {
     public float interactionRange = 10f; // 상호작용 가능한 거리
     public LayerMask interactableLayer; // 상호작용 가능한 레이어
-    Camera playerCamera;
+    Camera playerController;
 
     [SerializeField]
     InteractionManager interactionManager; // 상호작용 스크립트 총괄
+
+    [SerializeField] GameObject cameraHolder;
 
     private bool canInteract = false;
     private RaycastHit hitInfo;
 
     private void Start()
     {
-        playerCamera = GetComponentInChildren<Camera>();
+
     }
 
     private void Update()
@@ -28,7 +30,7 @@ public class PlayerRaycast : MonoBehaviour
     {
         Debug.DrawRay(transform.position, transform.forward * 10, Color.red);
         // 화면 중앙에서 Raycast 발사
-        Ray ray = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        Ray ray = cameraHolder.GetComponentInChildren<Camera>().ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
 
         if (Physics.Raycast(ray, out hitInfo, interactionRange, interactableLayer))
         {
