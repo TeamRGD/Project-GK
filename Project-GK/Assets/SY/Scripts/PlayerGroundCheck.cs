8ba89bdbe84ec203b59a68cb5397cb5178cc8b22
spoiuy3 <kuniyeoni@gmail.com>
2024-08-11 04:51:54 +0900

    Fix: PlayerGroundCheck

--- Project-GK/Assets/SY/Scripts/PlayerGroundCheck.cs
+++ Project-GK/Assets/SY/Scripts/PlayerGroundCheck.cs
@@ -13,49 +13,43 @@ public class PlayerGroundCheck : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Ground"))
-        {
-            playerController.SetGroundedState(true);
-        }
+        if (other.gameObject == playerController.gameObject)
+            return;
+        playerController.SetGroundedState(true);
     }
 
     void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Ground"))
-        {
-            playerController.SetGroundedState(false);   
-        }
+        if (other.gameObject == playerController.gameObject)
+            return;
+        playerController.SetGroundedState(false);
     }
 
     void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("Ground"))
-        {
-            playerController.SetGroundedState(true);
-        }
+        if (other.gameObject == playerController.gameObject)
+            return;
+        playerController.SetGroundedState(true);
     }
 
-    void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            playerController.SetGroundedState(true);
-        }
+        if (collision.gameObject == playerController.gameObject)
+            return;
+        playerController.SetGroundedState(true);
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            playerController.SetGroundedState(false);
-        }
+        if (collision.gameObject == playerController.gameObject)
+            return;
+        playerController.SetGroundedState(false);
     }
 
     private void OnCollisionStay(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            playerController.SetGroundedState(true);
-        }
+        if (collision.gameObject == playerController.gameObject)
+            return;
+        playerController.SetGroundedState(true);
     }
 }
