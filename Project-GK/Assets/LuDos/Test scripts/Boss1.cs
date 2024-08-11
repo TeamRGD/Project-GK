142bb4ebe5bc959415a8b25c90e3ac79878d2c41
Heokunho <93777460+Heokunho@users.noreply.github.com>
2024-08-11 19:49:07 +0900

    Fix: Boss Attack

--- Project-GK/Assets/LuDos/Test scripts/Boss1.cs
+++ Project-GK/Assets/LuDos/Test scripts/Boss1.cs
@@ -617,6 +617,8 @@ public class Boss1 : MonoBehaviourPunCallbacks
         animator.SetTrigger("PalmStrike"); // 1.97√ 
         yield return new WaitForSeconds(0.7f);
 
+        yield return new WaitForSeconds(3.0f);
+
         isExecutingAttack = false;
     }
 
