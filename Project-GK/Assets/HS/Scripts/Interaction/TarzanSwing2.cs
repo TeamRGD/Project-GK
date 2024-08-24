782e4986d98d20c02b429fffcd07731a1fd00229
CuriHuS <2542656@naver.com>
2024-08-24 12:47:33 +0900

    Fix: TarzanSwing2, Map

--- Project-GK/Assets/HS/Scripts/Interaction/TarzanSwing2.cs
+++ Project-GK/Assets/HS/Scripts/Interaction/TarzanSwing2.cs
@@ -100,7 +100,7 @@ public class TarzanSwing2 : MonoBehaviour
     {
         if(other.CompareTag("PlayerWi"))
         {
-            player = other.transform;
+            player = other.transform.root;
         }
     }
 }
