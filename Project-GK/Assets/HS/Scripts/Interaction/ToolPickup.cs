e1670d47ab28ee04b94a52a488ae1722d15f70d8
CuriHuS <2542656@naver.com>
2024-08-26 15:44:26 +0900

    Rename: Interaction Scripts
    상호작용 관련 스크립트 폴더 이동 및 수정

--- Project-GK/Assets/HS/Scripts/Interaction/ToolPickup.cs
+++ Project-GK/Assets/HS/Scripts/Interaction/ToolPickup.cs
@@ -7,13 +7,14 @@ public class ToolPickup : MonoBehaviour
     public GameObject toolPrefab; // 픽업할 도구 프리팹
     private Dictionary<PlayerController, PhotonView> players = new Dictionary<PlayerController, PhotonView>(); // 해당 오브젝트와 상호작용하는 Player를 담아 줌.
     PlayerToolManager playerToolManager;
+    PlayerController playerController;
+    PhotonView PV;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("PlayerWi") || other.CompareTag("PlayerZard"))
         {
-            PlayerController playerController;
-            PhotonView PV;
+            
             playerController = other.GetComponentInParent<PlayerController>();
             PV = playerController.GetComponentInParent<PhotonView>();
             if (!players.ContainsKey(playerController))
@@ -32,8 +33,6 @@ public class ToolPickup : MonoBehaviour
     {
         if (other.CompareTag("PlayerWi") || other.CompareTag("PlayerZard"))
         {
-            PlayerController playerController;
-            PhotonView PV;
             playerController = other.GetComponentInParent<PlayerController>();
             PV = playerController.GetComponentInParent<PhotonView>();
             if (players.ContainsKey(playerController))
