06adba49db57c8cd8f77e3866c5dc7f62fd48d84
spoiuy3 <kuniyeoni@gmail.com>
2024-08-28 14:53:06 +0900

    Revert "Refactor: PlayerGroundCheck"
    This reverts commit ef199c5c6abd3098ba8048e0bfd095e5cebbbf72.

--- Project-GK/Assets/SY/Scripts/PlayerGroundCheck.cs
+++ Project-GK/Assets/SY/Scripts/PlayerGroundCheck.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,78 +6,44 @@ public class PlayerGroundCheck : MonoBehaviour
 {
     PlayerController playerController;
 
-    // Tag 열거형 정의
-    public enum TagType
-    {
-        Ground,
-        Others,
-        Stair,
-        Note,
-        Note2,
-        BookPage2,
-        Book2,
-        Note3,
-        BookPage2_2,
-        BookPage2_3,
-        Puzzle3Cipher
-    }
-
     void Awake()
     {
         playerController = GetComponentInParent<PlayerController>();
     }
 
-    bool IsTagValid(string tag)
-    {
-        return Enum.TryParse(tag, out TagType validTag);
-    }
-
-    /* 무한점프 문제 구간 추정
     void OnTriggerEnter(Collider other)
     {
-        if (IsTagValid(other.tag))
-        {
-            playerController.SetGroundedState(true);
-        }
+        if (other.CompareTag("Ground") || other.CompareTag("Others"))
+        playerController.SetGroundedState(true);
     }
 
     void OnTriggerExit(Collider other)
     {
-        if (IsTagValid(other.tag))
-        {
-            playerController.SetGroundedState(false);
-        }
+        if(other.CompareTag("Ground") || other.CompareTag("Others"))
+        playerController.SetGroundedState(false);
     }
 
     void OnTriggerStay(Collider other)
     {
-        if (IsTagValid(other.tag))
-        {
-            playerController.SetGroundedState(true);
-        }
+        if(other.CompareTag("Ground") || other.CompareTag("Others"))
+        playerController.SetGroundedState(true);
     }
-    */
+
     private void OnCollisionEnter(Collision collision)
     {
-        if (IsTagValid(collision.gameObject.tag))
-        {
-            playerController.SetGroundedState(true);
-        }
+        if(collision.gameObject.CompareTag("Ground") || collision.collider.CompareTag("Others"))
+        playerController.SetGroundedState(true);
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        if (IsTagValid(collision.gameObject.tag))
-        {
-            playerController.SetGroundedState(false);
-        }
+        if(collision.gameObject.CompareTag("Ground") || collision.collider.CompareTag("Others"))
+        playerController.SetGroundedState(false);
     }
 
     private void OnCollisionStay(Collision collision)
     {
-        if (IsTagValid(collision.gameObject.tag))
-        {
-            playerController.SetGroundedState(true);
-        }
+        if(collision.gameObject.CompareTag("Ground") || collision.collider.CompareTag("Others"))
+        playerController.SetGroundedState(true);
     }
 }
