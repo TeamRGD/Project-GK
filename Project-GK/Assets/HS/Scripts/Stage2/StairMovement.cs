6567e257e9b01f713157b9f60435f071bdde59bc
CuriHuS <2542656@naver.com>
2024-09-01 19:48:48 +0900

    Fix: StairMovement script
    버튼 도중에 눌렀을 때 오류 수정

--- Project-GK/Assets/HS/Scripts/Stage2/StairMovement.cs
+++ Project-GK/Assets/HS/Scripts/Stage2/StairMovement.cs
@@ -4,28 +4,57 @@ using UnityEngine;
 public class StairMovement : MonoBehaviour
 {
     bool isAppear;
-    [SerializeField] private float moveDistance = 3f; // 이동할 거리
     [SerializeField] private float moveDuration = 1f; // 이동 시간
+    [SerializeField] Transform destination;
+    [SerializeField] Transform originTransform;
+
+    private Coroutine currentMoveCoroutine;
 
     public void Move()
     {
-        Vector3 targetPosition = isAppear ? transform.position - new Vector3(moveDistance, 0, 0) : transform.position + new Vector3(moveDistance, 0, 0);
-        StartCoroutine(MoveStair(targetPosition));
+        // 현재 이동 중인 코루틴이 있으면 중단
+        if (currentMoveCoroutine != null)
+        {
+
+            transform.position = isAppear ? originTransform.position : destination.position;
+
+            StopCoroutine(currentMoveCoroutine);
+        }
+
+
+        // 새로운 코루틴 시작
         isAppear = !isAppear;
+        currentMoveCoroutine = StartCoroutine(MoveStair());
     }
 
-    private IEnumerator MoveStair(Vector3 targetPosition)
+    private IEnumerator MoveStair()
     {
         Vector3 startPosition = transform.position;
         float elapsedTime = 0f;
 
-        while (elapsedTime < moveDuration)
+        if (isAppear)
         {
-            transform.position = Vector3.Lerp(startPosition, targetPosition, elapsedTime / moveDuration);
-            elapsedTime += Time.deltaTime;
-            yield return null;
+            while (elapsedTime < moveDuration)
+            {
+                transform.position = Vector3.Lerp(startPosition, destination.position, elapsedTime / moveDuration);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            transform.position = destination.position; // 이동 보정
+
+        }
+        else
+        {
+            while (elapsedTime < moveDuration)
+            {
+                transform.position = Vector3.Lerp(startPosition, originTransform.position, elapsedTime / moveDuration);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+            transform.position = originTransform.position; // 이동 보정
         }
 
-        transform.position = targetPosition; // 이동 보정
+        currentMoveCoroutine = null; // 코루틴이 끝났음을 표시
     }
 }
