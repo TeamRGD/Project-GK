1472e5b3aaa4627626a719a6f9a5c318cb511f0f
spoiuy3 <kuniyeoni@gmail.com>
2024-09-09 13:47:23 +0900

    Feat: Camera alt key

--- Project-GK/Assets/SY/Scripts/PlayerController.cs
+++ Project-GK/Assets/SY/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
@@ -35,13 +36,15 @@ public class PlayerController : MonoBehaviour
     bool canLook = true;
     bool canMove = true;
     bool isWalking = false;
-    public bool isSaving = true;
+    bool isSaving = true;
+    public bool isFreeLooking = false;
 
     // Other variable
     Vector3 smoothMoveVelocity;
     Vector3 moveAmount;
     float verticalLookRotation;
     public float currentSaveTime = 0.0f;
+    Quaternion originalCameraRotation;
 
     // Raycast variable
     [SerializeField] LayerMask interactableLayer;
@@ -195,26 +198,53 @@ public class PlayerController : MonoBehaviour
 
     void Look()
     {
+        if (Input.GetKeyDown(KeyCode.LeftAlt))
+        {
+            isFreeLooking = true;
+            originalCameraRotation = cameraHolder.transform.localRotation;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftAlt))
+        {
+            isFreeLooking = false;
+        }
         if (canLook)
         {
             MoveCamera();
-            MoveAimObject();
+            if (!isFreeLooking)
+            {
+                MoveAimObject();
+            }
         }
     }
 
-    void MoveCamera()
+    void MoveCamera() // Refactoring!!
     {
         // TPS (3인칭 시점)
         float horizontalRotation = Input.GetAxisRaw("Mouse X") * mouseSensitivity;
         float verticalRotation = Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
 
         // Player Body Rotation
-        playerBody.Rotate(Vector3.up * horizontalRotation);
+        if (!isFreeLooking)
+        {
+            playerBody.Rotate(Vector3.up * horizontalRotation);
+        }
 
         // Camera Rotation
         verticalLookRotation -= verticalRotation;
         verticalLookRotation = Mathf.Clamp(verticalLookRotation, -60f, 60f);
-        cameraHolder.transform.localEulerAngles = Vector3.right * verticalLookRotation;
+
+        if (!isFreeLooking)
+        {
+            // Alt 키가 눌리지 않았을 때는 기본 카메라 회전
+            cameraHolder.transform.localEulerAngles = Vector3.right * verticalLookRotation;
+        }
+        else
+        {
+            // Alt 키가 눌린 상태에서 카메라만 회전 (플레이어는 고정)
+            cameraHolder.transform.Rotate(Vector3.up * horizontalRotation);
+            cameraHolder.transform.localEulerAngles = new Vector3(verticalLookRotation, cameraHolder.transform.localEulerAngles.y, 0);
+        }
+        
         Vector3 playerPosition = playerHead.TransformPoint(new Vector3(-0.5f, 0, 0));
         Vector3 cameraPosition = playerPosition - cameraHolder.transform.forward * distanceFromPlayer;
 
@@ -226,17 +256,45 @@ public class PlayerController : MonoBehaviour
             UnityEngine.Debug.DrawLine(playerPosition, hit.point);
             if (hit.collider.CompareTag("Wall")||hit.collider.CompareTag("BookCase")||hit.collider.CompareTag("Ground")) // 벽과 충돌했을 경우
             {
-                cameraHolder.transform.position = Vector3.Lerp(cameraHolder.transform.position, hit.point + Vector3.up, 0.15f);
+                if (!isFreeLooking)
+                {
+                    cameraHolder.transform.position = Vector3.Lerp(cameraHolder.transform.position, hit.point + Vector3.up, 0.15f);
+                }
+                else
+                {
+                    cameraHolder.transform.position = hit.point + Vector3.up;
+                }
             }
             else // 벽외의 다른 것들과 충돌했을 경우 Player와 일정 거리 두기 <- 수정이 필요한가? 고민
             {
                 float clampedDistance = Mathf.Clamp(hitDistance, minDistanceFromPlayer, distanceFromPlayer);
-                cameraHolder.transform.position = Vector3.Lerp(cameraHolder.transform.position, playerPosition - cameraHolder.transform.forward * clampedDistance + Vector3.up, 0.03f); // 부드러운 움직임
+                if (!isFreeLooking)
+                {
+                    cameraHolder.transform.position = Vector3.Lerp(cameraHolder.transform.position, playerPosition - cameraHolder.transform.forward * clampedDistance + Vector3.up, 0.03f); // 부드러운 움직임
+                }
+                else
+                {
+                    cameraHolder.transform.position = playerPosition - cameraHolder.transform.forward * clampedDistance + Vector3.up; // 부드러운 움직임
+                }
             }
         }
         else
         {
-            cameraHolder.transform.position = Vector3.Lerp(cameraHolder.transform.position, cameraPosition + Vector3.up, 0.03f); // 부드러운 움직임
+            if (!isFreeLooking)
+            {
+                cameraHolder.transform.position = Vector3.Lerp(cameraHolder.transform.position, cameraPosition + Vector3.up, 0.03f); // 부드러운 움직임
+            }
+            else
+            {
+                cameraHolder.transform.position = cameraPosition + Vector3.up; // 부드러운 움직임
+            }
+        }
+
+        // Alt 키를 뗐을 때 카메라가 원래 회전으로 돌아감
+        // Have to fix
+        if (!isFreeLooking)
+        {
+            cameraHolder.transform.localRotation = Quaternion.Slerp(cameraHolder.transform.localRotation, originalCameraRotation, 0.01f);
         }
     }
 
