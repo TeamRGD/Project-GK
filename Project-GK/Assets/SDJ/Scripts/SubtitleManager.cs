37b2fd4953ce4693925485b0b817814ae3ea5cfe
Ozinger <chocosdj@naver.com>
2024-09-28 10:37:45 +0900

    Feat: Subtitle UI

---
+++ Project-GK/Assets/SDJ/Scripts/SubtitleManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
+
+public class SubtitleManager : MonoBehaviour
+{
+    public static SubtitleManager instance;
+
+    public TMP_Text subtitle;
+
+    public string[] testSubtitles;
+
+    private WaitForSeconds waitForNextSubtitle;
+    private WaitForSeconds waitForFadeOutSubtitle;
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        subtitle.gameObject.SetActive(false);
+        waitForNextSubtitle = new WaitForSeconds(1.8f);
+        waitForFadeOutSubtitle = new WaitForSeconds(0.2f);
+        StartCoroutine(SubtitleInitiate(testSubtitles));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator SubtitleInitiate(string[] subtitles)
+    {
+        subtitle.gameObject.SetActive(true);
+        for (int i = 0; i < subtitles.Length; i++)
+        {
+            subtitle.DOFade(1f, 0.2f).SetEase(Ease.OutSine);
+            subtitle.text = subtitles[i];
+            yield return waitForNextSubtitle;
+            subtitle.DOFade(0f, 0.2f).SetEase(Ease.InSine);
+            yield return waitForFadeOutSubtitle;
+        }
+        subtitle.gameObject.SetActive(false);
+    }
+}
