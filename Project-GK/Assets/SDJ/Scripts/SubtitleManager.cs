387a19899a73391cc0d1702a6ae7b1da40b7b13e
Ozinger <chocosdj@naver.com>
2024-11-21 12:39:20 +0900

    feat: Tooltip subtitle

--- Project-GK/Assets/SDJ/Scripts/SubtitleManager.cs
+++ Project-GK/Assets/SDJ/Scripts/SubtitleManager.cs
@@ -10,6 +10,7 @@ public class SubtitleManager : MonoBehaviour
     public static SubtitleManager instance;
 
     public TMP_Text subtitle;
+    public TMP_Text subtitleOnOff;
 
     public List<string> testSubtitles;
     public List<float> testWaitSeconds;
@@ -17,6 +18,8 @@ public class SubtitleManager : MonoBehaviour
 
     private WaitForSeconds waitForNextSubtitle;
     private WaitForSeconds waitForFadeOutSubtitle;
+
+    private bool isTalking;
     void Awake()
     {
         if (instance == null)
@@ -36,6 +39,8 @@ public class SubtitleManager : MonoBehaviour
         waitForNextSubtitle = new WaitForSeconds(1.8f);
         waitForFadeOutSubtitle = new WaitForSeconds(0.2f);
         StartCoroutine(SubtitleInitiate(testSubtitles, testWaitSeconds, testAudioSources));
+
+        isTalking = false;
     }
 
     // Update is called once per frame
@@ -46,9 +51,11 @@ public class SubtitleManager : MonoBehaviour
 
     public IEnumerator SubtitleInitiate(List<string> subtitles, List<float> waitSeconds, List<AudioSource> audios)
     {
+        if (!isTalking) yield return null;
         subtitle.gameObject.SetActive(true);
         for (int i = 0; i < subtitles.Count; i++)
         {
+            isTalking = true;
             audios[i].Play();
             subtitle.DOFade(1f, 0.2f).SetEase(Ease.OutSine);
             subtitle.text = subtitles[i];
@@ -56,6 +63,19 @@ public class SubtitleManager : MonoBehaviour
             subtitle.DOFade(0f, 0.2f).SetEase(Ease.InSine);
             yield return waitForFadeOutSubtitle;
         }
+        isTalking = false;
         subtitle.gameObject.SetActive(false);
+        yield return null;
+    }
+
+    public void EnableToolTip(string textScript)
+    {
+        subtitleOnOff.text = textScript;
+        subtitleOnOff.DOFade(1f, 0.3f).SetEase(Ease.OutSine);
+    }
+
+    public void DisableToolTip()
+    {
+        subtitleOnOff.DOFade(0f, 0.3f).SetEase(Ease.InSine);
     }
 }
