40a0e3abfae0b66ce3836466acec0fa5273f956d
CuriHuS <2542656@naver.com>
2024-11-27 03:24:29 +0900

    Refactor: 경비병
    경비병 애니메이션 추가 및 동기화 리팩토링

--- Project-GK/Assets/HS/Scripts/Stage0/GuardManager.cs
+++ Project-GK/Assets/HS/Scripts/Stage0/GuardManager.cs
@@ -4,28 +4,32 @@ using Photon.Pun;
 
 public class GuardManager : MonoBehaviour
 {
-
+    // 자막 관련 //
     [SerializeField] Subtitle subtitleS1_6_Wi; // 걸렸을 때 자막표기
     [SerializeField] Subtitle subtitleS1_6_Zard; // 걸렸을 때 자막표기
 
     [SerializeField] Subtitle subtitleRebirth_Wi; // 다시 태어날 때 자막 표기
     [SerializeField] Subtitle subtitleRebirth_Zard; // 다시 태어날 때 자막 표기
 
+    // 경비병 관련 //
+    Animator anim; // 경비병 애니메이션 컨트롤러
+    PhotonView photonView; // 
+
     public Transform[] patrolPoints; // 경비병이 순찰할 포인트들
     public float speed = 2f; // 경비병 이동 속도
     private int currentPointIndex = 0; // 현재 경비병이 목표로 하는 포인트
     private Transform targetPoint; // 다음 순찰 포인트
+    private Vector3 fixedPosition; // 경비병 고정 위치(플레이어 발견시) 
 
-    public float detectionTime = 2f; // 플레이어가 시야에 노출된 시간을 관리하는 변수
-    [SerializeField] private float playerDetectionCounterWi = 0f; // Wi가 시야에 있는 시간
-    [SerializeField] private float playerDetectionCounterZard = 0f; // Zard가 시야에 있는 시간
-    public float viewAngle = 360f; // 경비병의 시야각 (원형 시야)
-    public float sphereRadius = 1f; // SphereCast의 반경
-    public float immediateDistance = 2f; // 즉시 게임 종료 범위
-    public float gradualDistance = 5f; // 일정 시간 후 게임 종료 범위
-
+    public float sphereRadius = 12f; // 경비병 탐지반경
     public Transform rayOrigin; // Ray 발사 위치를 위한 자식 오브젝트 Transform
+    public bool playerInSight = false; // 플레이어가 시야에 있는지 여부
+    [SerializeField] private LayerMask detectionLayer; // 플레이어만 추출
 
+    private bool isTurning; // 경비병이 회전 중일 때 True
+    public float turnDuration = 0.8f; // 경비병 회전 시간
+
+    // 플레이어 관련 //
     private GameObject playerWi; // PlayerWi 태그를 가진 플레이어
     private GameObject playerZard; // PlayerZard 태그를 가진 플레이어
     PlayerController playerControllerWi;
@@ -33,12 +37,9 @@ public class GuardManager : MonoBehaviour
 
     public Transform resetLocationWi; // 게임 종료 시 PlayerWi가 이동할 위치
     public Transform resetLocationZard; // 게임 종료 시 PlayerZard가 이동할 위치
-    private bool playerInSight = false; // 플레이어가 시야에 있는지 여부
 
-    bool isEnding = false;
-
-    Animator anim;
-    PhotonView photonView;
+    // 퍼즐 관련 //
+    bool isEnding = false; // 경비병에게 걸렸을 때 True (End game 용)
 
     void Start()
     {
@@ -64,157 +65,119 @@ public class GuardManager : MonoBehaviour
 
     void Update()
     {
-        DetectPlayers();
+        DetectPlayer();
 
         // 플레이어가 시야 내에 없다면 순찰을 계속함
         if (!playerInSight)
         {
             Patrol();
         }
+
+        else
+        {
+            transform.position = fixedPosition;
+        }
     }
 
     void Patrol()
     {
+        if (isTurning)
+            return;
+
         if (Vector3.Distance(transform.position, targetPoint.position) < 0.2f)
         {
-            currentPointIndex = (currentPointIndex + 1) % patrolPoints.Length; // 다음 포인트로 순환
-            targetPoint = patrolPoints[currentPointIndex];
+            StartCoroutine(TurnAtPatrolPoint());
         }
-        transform.position = Vector3.MoveTowards(transform.position, targetPoint.position, speed * Time.deltaTime); // 경비병 이동
-        transform.LookAt(targetPoint); // 순찰 중 목표 지점 바라보기
 
-        playerDetectionCounterWi = 0f;
-        playerDetectionCounterZard = 0f;
-    }
-
-    void DetectPlayers()
-    {
-
-        // 두 플레이어 모두 감지
-        DetectSinglePlayerWi(playerWi);
-        if (playerZard != null)
-            DetectSinglePlayerZard(playerZard);
-    }
-
-    void DetectSinglePlayerWi(GameObject player)
-    {
-        if (player.tag != "PlayerWi") return; // 플레이어가 없는 경우 건너뜀
-
-        Vector3 directionToPlayer = player.transform.position - rayOrigin.position;
-        float distanceToPlayer = directionToPlayer.magnitude;
-
-        // 방향과 거리 계산
-        if (distanceToPlayer <= gradualDistance)
+        else
         {
-            // SphereCast를 통해 플레이어를 감지
-            RaycastHit hit;
-            if (Physics.SphereCast(rayOrigin.position, sphereRadius, directionToPlayer.normalized, out hit, gradualDistance))
-            {
-                if (hit.collider.CompareTag(player.tag))
-                {
-                    photonView.RPC("AnimationStateRPC", RpcTarget.AllBuffered, "isFind", true);
-                    playerInSight = true;
-                    // 즉시 게임 종료 범위 내에 있으면 바로 게임 종료
-                    if (distanceToPlayer <= immediateDistance)
-                    {
-                        if (!isEnding)
-                            StartCoroutine(EndGame(player));
-                        return;
-                    }
-                    // 점진적 감지 범위 내에 있으면 대기 후 게임 종료
-                    else if (distanceToPlayer <= gradualDistance)
-                    {
-                        playerInSight = true; // 플레이어가 시야 내에 있음
-                        playerDetectionCounterWi += Time.deltaTime; // 노출 시간 증가
+            transform.position = Vector3.MoveTowards(transform.position, targetPoint.position, speed * Time.deltaTime); // 경비병 이동
+            transform.LookAt(targetPoint); // 순찰 중 목표 지점 바라보기
+        }
+        
+    }
 
-                        if (playerDetectionCounterWi >= detectionTime) // 일정 시간 이상 노출되면
-                        {
-                            StartCoroutine(EndGame(player));
-                            return;
-                        }
-                    }
-                    else
-                    {
-                        playerDetectionCounterWi = 0f; // 플레이어가 감지되지 않으면 노출 시간 초기화
-                        photonView.RPC("AnimationStateRPC", RpcTarget.AllBuffered, "isFind", false);
-                        playerInSight = false;
-                    }
+    
+    IEnumerator TurnAtPatrolPoint()
+    {
+        isTurning = true;
+
+        // Turn 애니메이션 실행
+        photonView.RPC("AnimationTriggerRPC", RpcTarget.AllBuffered, "Turn");
+
+        // 일정 시간 동안 대기하며 회전
+        float elapsedTime = 0f;
+        while (elapsedTime < turnDuration)
+        {
+            transform.Rotate(0f, 180f * Time.deltaTime / turnDuration, 0f); // 일정 시간 동안 회전
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        // 다음 Patrol Point로 이동
+        currentPointIndex = (currentPointIndex + 1) % patrolPoints.Length;
+        targetPoint = patrolPoints[currentPointIndex];
+
+        isTurning = false;
+    } 
+
+    void DetectPlayer()
+    {
+        // 탐지 반경 안에 있는 모든 Collider를 가져옴
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, sphereRadius, detectionLayer);
+
+        if (hitColliders.Length > 0)
+        {
+            Debug.Log("플레이어가 주변에 있습니다!");
+            foreach (var hit in hitColliders)
+            {
+                playerInSight = true;
+                fixedPosition = transform.position;
+                photonView.RPC("AnimationStateRPC", RpcTarget.AllBuffered, "isFind", true);
+                if (!isEnding)
+                {
+                    photonView.RPC("EndGameRPC", RpcTarget.AllBuffered, hit.tag);
                 }
+                return;
             }
+        }
+        else if (!isEnding)
+        {
+            photonView.RPC("AnimationStateRPC", RpcTarget.AllBuffered, "isFind", false);
+            playerInSight = false;
         }
     }
 
     [PunRPC]
     public void AnimationStateRPC(string animationName, bool state)
     {
-        anim.SetBool(animationName, state);
+        if (anim != null)
+            anim.SetBool(animationName, state);
     }
 
-    void DetectSinglePlayerZard(GameObject player)
+    [PunRPC]
+    void AnimationTriggerRPC(string animation)
+    {   
+        Debug.Log("AnimationTriggerRPC 실행");
+        anim.SetTrigger(animation);
+    }
+
+    [PunRPC]
+    void EndGameRPC(string tag)
     {
-        if (player.tag != "PlayerZard") return;
-
-        Vector3 directionToPlayer = player.transform.position - rayOrigin.position;
-        float distanceToPlayer = directionToPlayer.magnitude;
-
-        // 방향과 거리 계산
-        if (distanceToPlayer <= gradualDistance)
-        {
-            // SphereCast를 통해 플레이어를 감지
-            RaycastHit hit;
-            if (Physics.SphereCast(rayOrigin.position, sphereRadius, directionToPlayer.normalized, out hit, gradualDistance))
-            {
-                if (hit.collider.CompareTag(player.tag))
-                {
-                    if (photonView != null)
-                        photonView.RPC("AnimationStateRPC", RpcTarget.AllBuffered, "isFind", true);
-                    else
-                        Debug.Log("PhotonView가 없습니다.");
-                    playerInSight = true;
-                    // 즉시 게임 종료 범위 내에 있으면 바로 게임 종료
-                    if (distanceToPlayer <= immediateDistance)
-                    {
-                        if (!isEnding)
-                        {
-                            StartCoroutine(EndGame(player));
-                        }
-                        return;
-                    }
-                    // 점진적 감지 범위 내에 있으면 대기 후 게임 종료
-                    else if (distanceToPlayer <= gradualDistance)
-                    {
-                        playerInSight = true; // 플레이어가 시야 내에 있음
-                        playerDetectionCounterZard += Time.deltaTime; // 노출 시간 증가
-
-                        if (playerDetectionCounterZard >= detectionTime) // 일정 시간 이상 노출되면
-                        {
-                            StartCoroutine(EndGame(player));
-                            return;
-                        }
-                    }
-                    else
-                    {
-                        playerDetectionCounterZard = 0f; // 플레이어가 감지되지 않으면 노출 시간 초기화
-                        photonView.RPC("AnimationStateRPC", RpcTarget.AllBuffered, "isFind", false);
-                        playerInSight = false;
-                    }
-                }
-            }
-        }
+        StartCoroutine(EndGame(tag));
     }
-
     
-    IEnumerator EndGame(GameObject player)
+    IEnumerator EndGame(string tag)
     {
         isEnding = true;
 
         playerControllerWi.SetCanMove(false);
-        if (playerControllerZard != null)
-            playerControllerZard.SetCanMove(false);
+        playerControllerZard.SetCanMove(false);
 
-        if (player.CompareTag("PlayerWi"))
+        if (tag == "PlayerWi")
             subtitleS1_6_Wi.LoopSubTitle();
-        else if (player.CompareTag("PlayerZard"))
+        else if (tag == "PlayerZard")
             subtitleS1_6_Zard.LoopSubTitle();
 
         yield return new WaitForSeconds(2f);
@@ -240,19 +203,15 @@ public class GuardManager : MonoBehaviour
         FadeInOut.instance.FadeIn(1f);
         yield return new WaitForSeconds(1f);
 
-        if (player.CompareTag("PlayerWi"))
+        if (tag == "PlayerWi")
             subtitleRebirth_Wi.LoopSubTitle();
-        else if (player.CompareTag("PlayerZard"))
+        else if (tag == "PlayerZard")
             subtitleRebirth_Zard.LoopSubTitle();
-
-        playerDetectionCounterWi = 0f;
-        playerDetectionCounterZard = 0f;
 
         isEnding = false;
 
         playerControllerWi.SetCanMove(true);
-        if (playerControllerZard != null)
-            playerControllerZard.SetCanMove(true);
+        playerControllerZard.SetCanMove(true);
 
         yield return null;
     }
@@ -264,10 +223,7 @@ public class GuardManager : MonoBehaviour
         Gizmos.color = Color.red;
 
         // SphereCast 시야를 시각화
-        Gizmos.DrawWireSphere(rayOrigin.position, gradualDistance);
+        Gizmos.DrawWireSphere(rayOrigin.position, sphereRadius);
 
-        // 즉시 종료 범위 시각화
-        Gizmos.color = Color.blue;
-        Gizmos.DrawWireSphere(rayOrigin.position, immediateDistance);
     }
 }
